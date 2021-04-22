605d390515d34935cf8a00606145863ce7ba36a3
Realdo-Justino <realdojj@gmail.com>
2021-04-22 13:59:37 -0300

    Exercicio 03

---
+++ exercicio_03.py
@@ -0,0 +1,17 @@
+class aluno:pass
+
+Realdo=aluno()
+Otavio=aluno()
+
+Realdo.nome="Realdo justino Júnior"
+Otavio.nome="Otavio Beirniard Correa"
+
+Realdo.cpf=3432423423212
+Otavio.cpf=8744323888292
+
+Realdo.telefone=32432432
+Otavio.telefone=67236776
+
+print("Nome: ",Realdo.nome,"\nCPF: ",Realdo.cpf,"\nTelefone: ",Realdo.telefone)
+print("\n\n")
+print("Nome: ",Otavio.nome,"\nCPF: ",Otavio.cpf,"\nTelefone: ",Otavio.telefone)
