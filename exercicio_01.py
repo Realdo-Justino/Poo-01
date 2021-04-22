99901d59176cfbbb58d6acd0678673973630ecaa
Realdo-Justino <realdojj@gmail.com>
2021-04-22 13:56:35 -0300

    Exercicio 01

---
+++ exercicio_01.py
@@ -0,0 +1,29 @@
+class Carro:
+    pass
+ 
+my_car = Carro()
+your_car = Carro()
+ 
+my_car.ano = 2020
+ 
+my_car.cor = 'branco'
+ 
+my_car.modelo = 'ferrari'
+ 
+your_car.modelo = 'roxo'
+ 
+your_car.ano = 1970
+ 
+your_car.modelo = 'fusca'
+ 
+your_car.modelo
+ 
+your_car.cor = 'roxo'
+ 
+your_car.ano += 12
+ 
+your_car.ano
+ 
+print(my_car)
+ 
+print(my_car.ano, my_car.modelo)
