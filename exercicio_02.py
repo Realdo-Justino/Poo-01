3ae3b4d20f4996f5a1347ffc13b49a30216ff7fc
Realdo-Justino <realdojj@gmail.com>
2021-04-22 13:58:55 -0300

    Exercicio 02

---
+++ exercicio_02.py
@@ -0,0 +1,17 @@
+class moto:pass
+
+minha_moto=moto()
+sua_moto=moto()
+
+minha_moto.cor="preta"
+sua_moto.cor="cinza"
+
+minha_moto.ano=1987
+sua_moto.ano=2013
+
+minha_moto.km=12000
+sua_moto.km=1000
+
+print("Minha moto\nCor",minha_moto.cor,"\nAno",minha_moto.ano,"\nKm",minha_moto.km)
+print("\n\n")
+print("Sua moto\nCor",sua_moto.cor,"\nAno",sua_moto.ano,"\nKm",sua_moto.km)
